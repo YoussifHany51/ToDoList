42c8ad1ff09c17ec055fc256d66f6815d727a4cc
Youssif Hany <youssifhany962@gmail.com>
2022-02-04 16:38:18 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Youssif Hany on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
