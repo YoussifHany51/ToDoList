41ef2bb06a7783605bea4304e214d9bc0ee95d3d
Youssif Hany <youssifhany962@gmail.com>
2022-02-08 23:21:27 +0200

    launchScreen Added

---
+++ ToDoList/views/NoTasks.swift
@@ -0,0 +1,20 @@
+//
+//  NoTasks.swift
+//  ToDoList
+//
+//  Created by Youssif Hany on 08/02/2022.
+//
+
+import SwiftUI
+
+struct NoTasks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoTasks_Previews: PreviewProvider {
+    static var previews: some View {
+        NoTasks()
+    }
+}
